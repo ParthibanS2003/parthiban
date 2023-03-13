3a614be4ac67f07206fe59ce5c81cc6ecbdfcd17
ParthibanS2003 <123487519+ParthibanS2003@users.noreply.github.com>
2023-03-13 19:23:05 +0530

    Add files via upload

---
+++ php/db.php
@@ -0,0 +1,18 @@
+<?php
+
+// MySQL database configuration
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'saveetha');
+define('DB_NAME', 'parthiban');
+
+// MongoDB database configuration
+define('MONGODB_HOST', 'mongodb://localhost:27017');
+define('MONGODB_USERNAME', 'your_mongodb_username');
+define('MONGODB_PASSWORD', 'your_mongodb_password');
+define('MONGODB_NAME', 'your_mongodb_database_name');
+
+// Redis database configuration
+define('REDIS_HOST', '127.0.0.1');
+define('REDIS_PORT', 6379);
+define('REDIS_PASSWORD', 'your_redis_password');
